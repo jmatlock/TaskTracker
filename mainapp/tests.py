17703fc61ca7ba3c565894a0066254aa62b78243
jmatlock <155165+jmatlock@users.noreply.github.com>
2021-01-13 16:58:35 -0500

    Added view for Task; template to show Task data; Bootstrap for styling; initial tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Task, Category, Location, WorkItem
 
 # Create your tests here.
+
+class SimpleWebTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        Location.objects.create(name='Garage')
+        Category.objects.create(name='Declutter')
+        Task.objects.create(summary='Declutter garage',
+                            details='Declutter garage',
+                            category=Category.objects.get(id=1),
+                            location=Location.objects.get(id=1))
+        # TODO: add WorkItem to setUp
+
+    def test_task_content(self):
+        task = Task.objects.get(id=1)
+        expected_object_name = f'{task.summary}'
+        self.assertEqual(expected_object_name, 'Declutter garage')
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Task
 
 # Create your views here.
+class HomePageView(ListView):
+    model = Task
+    template_name = 'index.html'
+    context_object_name = 'all_tasks_list'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
